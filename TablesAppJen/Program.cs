6f01ffcf1477fce13bee3e4d6cf26006e00ab06f
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-15 12:55:17 +0530

    Add project files.

---
+++ TablesAppJen/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Table of 5");
+int num = 5;
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine("{0,3} * {1,3} = {2,3}",i,num,(i*num));
+}
